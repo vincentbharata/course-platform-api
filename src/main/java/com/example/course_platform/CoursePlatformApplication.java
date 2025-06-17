053e9ffd5aad2b714db1bbe2cddb30103f359379
vincentbharata <vincentbharataa@gmail.com>
2025-06-17 13:46:24 +0700

    init

---
+++ src/main/java/com/example/course_platform/CoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApplication.class, args);
+	}
+
+}
